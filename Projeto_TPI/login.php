5030d812e77ab1b8f6776d6665e0ea29ac56cf4a
Oliveira555 <goncalooliveira003@gmail.com>
2024-01-17 17:10:03 +0000

    17/1/2024
    aaa

---
+++ Projeto_TPI/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles_login.css">
+    <title> V0ID Login </title>
+
+</head>
+<body>
+    <div>
+        <h1> Login </h1>
+    <form action="testlogin.php" method="POST">
+        <input type="text" name="nome" placeholder="Username">
+        <br><br>
+
+        <input type="password" name="senha" placeholder="Password">
+        <br><br>
+        <input class="inputSubmit" type="submit" name="submit" value="Login">
+
+
+
+
+
+
+
+    </form>
+</div>
+
+
+
+
+</body>
+</html>
---
+++ Projeto_TPI/register.php
@@ -0,0 +1,55 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+        print_r($_POST['nome']);
+        print_r($_POST['senha']);
+
+
+
+        $nome = $_POST['nome'];
+        $senha = $_POST['senha'];
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles_register.css">
+    <title> V0ID Register </title>
+
+</head>
+<body>
+    <div class="box">
+        <form action="register.php" method="POST">
+            <fieldset>
+                <legend><b> Register </b></legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="nome" id="nome" class="inputUser" required>
+                    <label for="nome" class="labelInput"> Username </label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="password" name="senha" id="senha" class="inputUser" required>
+                    <label for="senha" class="labelInput"> Password </label>
+                </div>
+                
+                <br><br>
+                <input type="submit" name="submit" id="submit">
+            </fieldset>
+            <div class="already-account">
+                <p> Already have an account? <a href="login.html"> <span> Login </span></a></p>
+            </div>
+            <div class="go-back">
+                <span><a href="Index.html"> GO BACK </a></span> 
+            </div>
+        </form>
+    </div>
+</body>
+</html>
