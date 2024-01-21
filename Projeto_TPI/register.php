fae49aaef35b8dec583e48ca97931cdf6aab82e8
JoaoDuarte0603 <joao.m.duarte04@gmail.com>
2024-01-21 14:33:32 +0000

    ...

--- Projeto_TPI/register.php
+++ Projeto_TPI/register.php
@@ -34,7 +34,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles_register.css">
-    <link rel="icon" href="../images\favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../Projeto_TPI\images\favicon.ico" type="image/x-icon">
     <title>V0ID Clothing</title>
 </head>
 <body>
